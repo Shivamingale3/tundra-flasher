bfaf807f4f368930575fbcd3b37071803272f53a
Shivamingale3 <shivamingale3@gmail.com>
2024-05-23 00:42:34 +0530

    added
    Signed-off-by: Shivamingale3 <shivamingale3@gmail.com>

---
+++ command_runner.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+
+def run_command(command):
+    try:
+        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        return f"Error executing command: {e}\n{e.stderr}"
